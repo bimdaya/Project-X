84fee32c14ea772e24f0c090619b0f37fc54670c
bud93411 <bimalidayananda@gmail.com>
2019-05-04 17:19:20 +0300

    SQUAD-9-313 Removing 'request-promise-native' since it is not used in the module

--- test/integration/clients.test.js
+++ test/integration/clients.test.js
@@ -1,7 +1,6 @@
 const chai = require('chai');
 
 const chaiHttp = require('chai-http');
-const rp = require('request-promise-native');
 const config = require('../helpers/config');
 
 const { assert, expect } = chai;
@@ -99,4 +98,5 @@ describe('Should handle client record requests ', function() {
       done();
     });
   });
+
 });
