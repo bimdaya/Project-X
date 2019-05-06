ede817092f5b696f94f079b9dd0038c696ffee62
bud93411 <bimalidayananda@gmail.com>
2019-05-06 15:04:44 +0300

    SQUAD-9-313 Fixing indentation issues in unit test files

--- test/unit/controllers/clientsController.test.js
+++ test/unit/controllers/clientsController.test.js
@@ -6,113 +6,122 @@ const { expect } = chai;
 const clientId = 'test-client-id';
 
 describe('clientsController', () => {
-	afterEach = () =>  {
-		td.reset()
-	}
+  afterEach = () => {
+    td.reset()
+  }
 
-	it('getAllClients should return a list of clients', async () => {
-		const clientsList = [
-			{ id: clientId }
-		]
+  it('getAllClients should return a list of clients', async () => {
+    const clientsList = [{
+      id: clientId
+    }]
 
-		const clientModel = td.replace('../../../src/models/ClientModel');
-		td.when(clientModel.getClientsList()).thenResolve(clientsList);
+    const clientModel = td.replace('../../../src/models/ClientModel');
+    td.when(clientModel.getClientsList()).thenResolve(clientsList);
 
-		const ClientsController =
-			require('../../../src/controllers/ClientsController');
-		const getAllClientsResult = await ClientsController.getAllClients();
+    const ClientsController =
+      require('../../../src/controllers/ClientsController');
+    const getAllClientsResult = await ClientsController.getAllClients();
 
-		expect(getAllClientsResult)
-			.to.be.an('array')
-			.that.equals(clientsList)
-	});
+    expect(getAllClientsResult)
+      .to.be.an('array')
+      .that.equals(clientsList)
+  });
 
-	it ('getClientById should return one client', async () => {
-		const clientModel = td.replace('../../../src/models/ClientModel');
-		td.when(clientModel.getClientById(clientId)).thenResolve({ id: clientId });
-
-		const ClientsController =
-			require('../../../src/controllers/ClientsController');
-		const getClientByIdResult =
-			await ClientsController.getClientById({ params: { clientId: clientId }});
-
-		expect(getClientByIdResult)
-			.to.be.an('object')
-			.and.has.property('id')
-			.that.equals(clientId)
-	});
-
-	it ('createClient should create one client', async () => {
-		const req = {
-			body: {
-				phoneNumber: '+4407777712333',
-				firstName: 'John',
-				surname: 'Doe',
-			}
-		}
-
-		const validator = td.replace('../../../src/helpers/validator');
-		td.when(validator.validate(td.matchers.isA(String), req.body))
-			.thenReturn({ valid: true });
-
-		const clientModel = td.replace('../../../src/models/ClientModel');
-		td.when(clientModel.createClient(req.body))
+  it('getClientById should return one client', async () => {
+    const clientModel = td.replace('../../../src/models/ClientModel');
+    td.when(clientModel.getClientById(clientId))
 			.thenResolve({ id: clientId });
 
-		const ClientsController =
-			require('../../../src/controllers/ClientsController');
-		const createOneResult = await ClientsController.createClient(req);
-
-		expect(createOneResult)
-			.to.be.an('object')
-			.and.has.property('id')
-			.that.is.an('string')
-	});
-
-	it ('deleteClientById should delete client', async () => {
-		const clientModel = td.replace('../../../src/models/ClientModel');
-		td.when(clientModel.getClientById(clientId)).thenResolve({ id: clientId });
-
-		const ClientsController =
-			require('../../../src/controllers/ClientsController');
-		const deleteOneResult = await ClientsController
-			.deleteClientById({ params: { clientId: clientId }});
-
-		expect(deleteOneResult)
-			.to.be.an('object')
-			.and.has.property('message')
-			.that.is.an('string')
-			.that.equals(`Client Id: ${clientId} deleted successfully.`)
-	});
-
-	it ('updateClient should update client', async () => {
-		const clientModel = td.replace('../../../src/models/ClientModel');
-		td.when(clientModel.getClientById(clientId))
-			.thenResolve({ id: clientId });
-
-		const req = {
-			body: {
-				firstname: 'John',
-				surname: 'Doe',
-			},
+    const ClientsController =
+      require('../../../src/controllers/ClientsController');
+    const getClientByIdResult = await ClientsController.getClientById({
 			params: {
-				clientId: clientId
-			}
-		}
+          clientId: clientId
+        }
+      });
 
-		const validator = td.replace('../../../src/helpers/validator');
-		td.when(validator.validate(td.matchers.isA(String), req.body))
-			.thenReturn({ valid: true });
+    expect(getClientByIdResult)
+      .to.be.an('object')
+      .and.has.property('id')
+      .that.equals(clientId)
+  });
 
-		const ClientsController =
-			require('../../../src/controllers/ClientsController');
-		const updateOneResult = await ClientsController.updateClient(req);
+  it('createClient should create one client', async () => {
+    const req = {
+      body: {
+        phoneNumber: '+4407777712333',
+        firstName: 'John',
+        surname: 'Doe',
+      }
+    }
 
-		expect(updateOneResult)
-			.to.be.an('object')
-			.and.has.property('message')
-			.that.is.an('string')
-			.that.equals(`Client Id: ${clientId} updated successfully.`)
-	});
+    const validator = td.replace('../../../src/helpers/validator');
+    td.when(validator.validate(td.matchers.isA(String), req.body))
+      .thenReturn({ valid: true });
+
+    const clientModel = td.replace('../../../src/models/ClientModel');
+    td.when(clientModel.createClient(req.body))
+      .thenResolve({ id: clientId });
+
+    const ClientsController =
+      require('../../../src/controllers/ClientsController');
+    const createOneResult = await ClientsController.createClient(req);
+
+    expect(createOneResult)
+      .to.be.an('object')
+      .and.has.property('id')
+      .that.is.an('string')
+  });
+
+  it('deleteClientById should delete client', async () => {
+    const clientModel = td.replace('../../../src/models/ClientModel');
+    td.when(clientModel.getClientById(clientId))
+			.thenResolve({ id: clientId });
+
+    const ClientsController =
+      require('../../../src/controllers/ClientsController');
+    const deleteOneResult = await ClientsController
+      .deleteClientById({
+        params: {
+          clientId: clientId
+        }
+      });
+
+    expect(deleteOneResult)
+      .to.be.an('object')
+      .and.has.property('message')
+      .that.is.an('string')
+      .that.equals(`Client Id: ${clientId} deleted successfully.`)
+  });
+
+  it('updateClient should update client', async () => {
+    const clientModel = td.replace('../../../src/models/ClientModel');
+    td.when(clientModel.getClientById(clientId))
+      .thenResolve({ id: clientId });
+
+    const req = {
+      body: {
+        firstname: 'John',
+        surname: 'Doe',
+      },
+      params: {
+        clientId: clientId
+      }
+    }
+
+    const validator = td.replace('../../../src/helpers/validator');
+    td.when(validator.validate(td.matchers.isA(String), req.body))
+      .thenReturn({ valid: true });
+
+    const ClientsController =
+      require('../../../src/controllers/ClientsController');
+    const updateOneResult = await ClientsController.updateClient(req);
+
+    expect(updateOneResult)
+      .to.be.an('object')
+      .and.has.property('message')
+      .that.is.an('string')
+      .that.equals(`Client Id: ${clientId} updated successfully.`)
+  });
 
 });
--- test/unit/model/clientModel.test.js
+++ test/unit/model/clientModel.test.js
@@ -10,24 +10,24 @@ describe('clientModel', () => {
   const db = td.replace('../../../src/helpers/postgres');
   const clientModel = require('../../../src/models/ClientModel');
 
-	afterEach = () =>  {
-		td.reset()
-	}
+  afterEach = () => {
+    td.reset()
+  }
 
   it('getClientsList should return a list of clients', async () => {
-		const clientsList = [
-			{ id: clientId }
-		];
+    const clientsList = [{
+      id: clientId
+    }];
 
-		td.when(db.any(queries.clients.get, {})).thenResolve(clientsList);
-		const getAllClientsResult = await clientModel.getClientsList();
+    td.when(db.any(queries.clients.get, {})).thenResolve(clientsList);
+    const getAllClientsResult = await clientModel.getClientsList();
 
-		expect(getAllClientsResult)
-			.to.be.an('array')
-			.that.equals(clientsList)
-	});
+    expect(getAllClientsResult)
+      .to.be.an('array')
+      .that.equals(clientsList)
+  });
 
-  it ('getClientById should return one client', async () => {
+  it('getClientById should return one client', async () => {
     const client = {
       id: clientId
     };
@@ -41,47 +41,49 @@ describe('clientModel', () => {
       .that.equals(client.id)
   });
 
-	it ('createClient should create one client', async () => {
-		const req = {
-			body: {
-				phoneNumber: '+4407777712333',
-				firstName: 'John',
-				surname: 'Doe',
-			}
-		};
+  it('createClient should create one client', async () => {
+    const req = {
+      body: {
+        phoneNumber: '+4407777712333',
+        firstName: 'John',
+        surname: 'Doe',
+      }
+    };
     const client = {
       client: clientId
     };
 
     td.when(db.one(queries.clients.createOne, req.body)).thenResolve(client);
-		const createOneResult = await clientModel.createClient(req.body);
+    const createOneResult = await clientModel.createClient(req.body);
 
-		expect(createOneResult)
-			.to.be.an('object')
-			.and.has.property('id')
-			.that.is.an('string')
-	});
+    expect(createOneResult)
+      .to.be.an('object')
+      .and.has.property('id')
+      .that.is.an('string')
+  });
 
-	it ('deleteById should delete client', async () => {
-		td.when(db.none(queries.clients.deleteOne, { clientId })).thenResolve(undefined);
-		const deleteOneResult = await clientModel.deleteById(clientId);
+  it('deleteById should delete client', async () => {
+    td.when(db.none(queries.clients.deleteOne, { clientId }))
+      .thenResolve(undefined);
+    const deleteOneResult = await clientModel.deleteById(clientId);
 
-		expect(deleteOneResult)
-			.to.be.undefined
-	});
+    expect(deleteOneResult)
+      .to.be.undefined
+  });
 
-	it ('updateById should update client in the database', async () => {
-		const clientData = {
-				firstname: 'John',
-				surname: 'Doe',
-        clientId: clientId
-		}
+  it('updateById should update client in the database', async () => {
+    const clientData = {
+      firstname: 'John',
+      surname: 'Doe',
+      clientId: clientId
+    }
 
-    td.when(db.none(queries.clients.updateOne, clientData)).thenResolve(undefined);
-		const updateOneResult = await clientModel.updateById(clientData);
+    td.when(db.none(queries.clients.updateOne, clientData))
+      .thenResolve(undefined);
+    const updateOneResult = await clientModel.updateById(clientData);
 
-		expect(updateOneResult)
-			.to.be.undefined
-	});
+    expect(updateOneResult)
+      .to.be.undefined
+  });
 
 });
